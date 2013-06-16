9fd0f111da444e49116853cdd17d9eb8b9371662
Nils Buehner <nils.buehner@gmail.com>
2013-06-16 19:00:02 +0200

    added ContentInitializer and made some refactoring

---
+++ src/main/java/de/rixtrick/init/ContentInitializer.java
@@ -0,0 +1,45 @@
+package de.rixtrick.init;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/**
+ * @author Nils Buehner
+ * 
+ *         Class to initialize some kind of content
+ * 
+ */
+public class ContentInitializer {
+
+	/**
+	 * The Logger
+	 */
+	private static Logger LOGGER = Logger.getLogger(ContentInitializer.class);
+
+	/**
+	 * Flag symbolizing if something should be initialized by this class
+	 */
+	private Boolean contentInitEnabled;
+
+	/**
+	 * The method called on initialization
+	 */
+	public void initializeDatabaseContent() {
+		if (this.contentInitEnabled.equals(true)) {
+			LOGGER.info("INITIALIZING DATABASE OR SOMETHING!");
+		} else {
+			LOGGER.info("NOT INITIALIZING ANYTHING!");
+		}
+	}
+
+	/**
+	 * @param contentInitEnabled
+	 *            if some content should be initialized or not
+	 */
+	@Autowired
+	@Qualifier("contentInitEnabled")
+	public void setContentInitEnabled(Boolean contentInitEnabled) {
+		this.contentInitEnabled = contentInitEnabled;
+	}
+}
